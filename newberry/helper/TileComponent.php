cefcfdb80d9c46b07bac15f6f0059922d2b3cadb
N W <nick33white@gmail.com>
2024-06-04 17:05:45 -0500

    add tile component

--- newberry/helper/TileComponent.php
+++ newberry/helper/TileComponent.php
@@ -5,8 +5,46 @@ use Laminas\View\Helper\AbstractHelper;
 
 class TileComponent extends AbstractHelper
 {
-    public function __invoke($progress = null) 
-    {
-        return "rgba({$progress}.08)";
+  public function __invoke($title = null, $linkUrl = null, $imageUrl = null, $progress = null, $filterString = null) 
+  {
+    if (is_string($progress)) {
+
+    $progColor = ($progress === "Ready for Review") ?  "green" : "red" ;
     }
+
+$progressContent = "";
+
+if (is_numeric($progress)) {
+  $progressContent = <<<HTML
+    <h4>$title</h4>
+    <div class="progress-bar" 
+      style="background-image: 
+        linear-gradient(to right, 
+          var(--progress-green) 0%, 
+          var(--progress-green) $progress%, 
+          var(--progress-bg) $progress%, 
+          var(--progress-bg) 100%
+        );">
+      <span>$progress% Complete</span>
+    </div>
+  HTML;
+} else {
+  $progressContent = <<<HTML
+    <div class="page progress-bar $progColor">
+      <span>$progress</span>
+    </div>
+  HTML;
+}
+
+$html = <<<HTML
+<a href="$linkUrl" class="tile" data-subjects="$filterString[0]"
+  data-language="$filterString[1]" data-format="$filterString[2]"
+  data-dates="$filterString[3]">
+  <img src="$imageUrl" alt="$title" />
+  $progressContent
+</a>
+HTML;
+
+    return $html;
+  }
 }
