1430bf806bb8def5fa76954c36b21a60b3c570ef
N W <nick33white@gmail.com>
2024-06-05 16:57:21 -0500

    continue work on creator page

--- newberry/helper/TileComponent.php
+++ newberry/helper/TileComponent.php
@@ -5,45 +5,37 @@ use Laminas\View\Helper\AbstractHelper;
 
 class TileComponent extends AbstractHelper
 {
-  public function __invoke($title = null, $linkUrl = null, $imageUrl = null, $progress = null, $filterString = null) 
+  public function __invoke( $tileData ) 
   {
+    ['tileType' => $tileType, 'title' => $title, 'linkUrl' => $linkUrl, 'imageUrl' => $imageUrl, 'progress' => $progress, 'filterString' => $filterString, 'detail' => $detail] = $tileData;
     if (is_string($progress)) {
-
-    $progColor = ($progress === "Ready for Review") ?  "green" : "red" ;
+      $progColor = ($progress === "Ready for Review") ?  "green" : "red" ;
+    } else { 
+      $progColor = "green";
     }
 
-$progressContent = "";
+    $progressText = is_string($progress) ? $progress : "{$progress}% Complete";
+    $progressPercent = is_string($progress) ? "100" : $progress;
 
-if (is_numeric($progress)) {
-  $progressContent = <<<HTML
-    <h4>$title</h4>
-    <div class="progress-bar" 
-      style="background-image: 
-        linear-gradient(to right, 
-          var(--progress-green) 0%, 
-          var(--progress-green) $progress%, 
-          var(--progress-bg) $progress%, 
-          var(--progress-bg) 100%
-        );">
-      <span>$progress% Complete</span>
-    </div>
-  HTML;
-} else {
-  $progressContent = <<<HTML
-    <div class="page progress-bar $progColor">
-      <span>$progress</span>
-    </div>
-  HTML;
-}
-
-$html = <<<HTML
-<a href="$linkUrl" class="tile" data-subjects="$filterString[0]"
-  data-language="$filterString[1]" data-format="$filterString[2]"
-  data-dates="$filterString[3]">
-  <img src="$imageUrl" alt="$title" />
-  $progressContent
-</a>
-HTML;
+    $html = <<<HTML
+      <a href="$linkUrl" class="tile $tileType $progColor" data-subjects="$filterString[0]"
+        data-language="$filterString[1]" data-format="$filterString[2]"
+        data-dates="$filterString[3]">
+        <img src="$imageUrl" alt="$title" />
+          <h4 class="title">$title</h4>
+          <p class="detail">$detail</p>
+          <div class="progress-bar" 
+            style="background-image: 
+              linear-gradient(to right, 
+                var(--progress-fg) 0%, 
+                var(--progress-fg) $progressPercent%, 
+                var(--progress-bg) $progressPercent%, 
+                var(--progress-bg) 100%
+              );">
+            <span>$progressText</span>
+          </div>
+      </a>
+    HTML;
 
     return $html;
   }
